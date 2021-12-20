da60b37eee99731464a02cdd222ddab1bc613e64
TheProdigy161 <theprodigy161@yahoo.co.uk>
2021-12-20 22:06:15 +0000

    Updated to make sure that server can run

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import http from 'http';
+import { Server } from 'socket.io';
 
-const PORT = 5000;
+const PORT = 5001;
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.use(cors());
+// app.use(cors());
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+    console.log('User connected.');
+});
+
+server.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
 });
