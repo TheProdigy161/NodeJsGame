798df8c7ab2733878e07c68fee835ac6bb3995fd
Ross Bratton <RossB0918@randox.com>
2021-12-20 15:56:00 +0000

    Updated to add TicTacToeComponent to routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
+import { TicTacToeComponent } from './components/tic-tac-toe/tic-tac-toe.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
+  { path: '**', component: TicTacToeComponent }
 ];
 
 @NgModule({
