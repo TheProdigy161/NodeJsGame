bd75e47fa6471c4fa24e336e0cb1291f68c09059
Ross Bratton <RossB0918@randox.com>
2022-01-05 15:16:54 +0000

    Updated to remove game list for now

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
+import { TicTacToeCreateRoomComponent } from './components/tic-tac-toe-create-room/tic-tac-toe-create-room.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TicTacToeOnlineComponent } from './components/tic-tac-toe-online/tic-tac-toe-online.component';
 import { TicTacToeRoomListComponent } from './components/tic-tac-toe-room-list/tic-tac-toe-room-list.component';
 
 const routes: Routes = [
-  { path: 'tictactoe', component: TicTacToeRoomListComponent },
+  // { path: 'tictactoe', component: TicTacToeRoomListComponent },
+  { path: 'tictactoe/game', component: TicTacToeCreateRoomComponent },
   { path: 'tictactoe/game/:gameCode', component: TicTacToeOnlineComponent },
   { path: '**', redirectTo: 'tictactoe' },
 ];
